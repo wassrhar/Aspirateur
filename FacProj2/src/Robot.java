2e230cd3a59023d5f0baab4985d49aab78b7b837
Spazzou <dropbox_wass@hotmail.fr>
2015-04-14 23:30:33 +0200

    Y

--- FacProj2/src/Robot.java
+++ FacProj2/src/Robot.java
@@ -111,7 +111,11 @@ public class Robot extends Thread{
 		Interface.CarteRobot.positionsToCarte(vectorPourAffichage(), Piece.getDimensions()[0]*Piece.getDimensions()[1], Piece.getDimensions()[1], Piece.getDimensions()[0]);
 		Interface.CarteRobot.affichageAspirateur(positionCourante.getX(), positionCourante.getY());
 		Interface.CarteRobot.repaint();
-		Interface.LabelValeurBatterie.setText(Float.toString((capaciteBatterie/capaciteTotalBatterie)*100).substring(0, 3));
+		String toDisplay=Float.toString((capaciteBatterie/capaciteTotalBatterie)*100);
+		if(toDisplay.length()>4){
+			toDisplay=toDisplay.substring(0, 4);
+		}
+		Interface.LabelValeurBatterie.setText(toDisplay);
 		Interface.LabelValeurReservoir.setText(Integer.toString(etatReserve));
 		Interface.LabelValeurTemps.setText(Long.toString(Chrono.Stop_Chrono()/1000));
 		Interface.LabelValeurMetrage.setText(Integer.toString(metrage));
@@ -129,7 +133,7 @@ public class Robot extends Thread{
 		setAspire(true);
 		while(!Piece.estPropre){
 			setAspire(true);
-			ResultatRecurrence res=determinerPlusCourtCheminBase(positionCourante.getX(),positionCourante.getY(),new ArrayList<PositionParcouru>());
+			ResultatRecurrence res=determinerPlusCourtCheminBase(positionCourante.getX(),positionCourante.getY(),new ArrayList<PositionParcouru>(),positionCourante.getX(),positionCourante.getY());
 
 			bouger();
 			
@@ -206,12 +210,205 @@ public class Robot extends Thread{
 		}
 		return current;
 	}
+	
+	private int qtePoussiereNonTraite(){
+		int qtePoussiere=0;
+		for(int i=0;i<positionParcouru.size();i++){
+			qtePoussiere=qtePoussiere+Piece.getPosition(positionParcouru.get(i).getX(), positionParcouru.get(i).getY()).getQtePoussiere();
+		}
+		
+		return qtePoussiere;
+	}
 	/**Permet au robot de bouger d'une case dans son sens de direction.
 	 * Si la case n'est pas accessible, change le sens de direction du robot avant de le déplacer.
 	 * @throws outOfEnergy 
 	 * @throws InterruptedException 
 	 */
 	public void bouger() throws InterruptedException, outOfEnergy{
+		int currentX=positionCourante.getX();
+		int currentY=positionCourante.getY();
+		if(positionParcouru.size()>1){ 
+			if(qtePoussiereNonTraite()>0){
+				int index=-2;
+				int distance=Piece.getDimensions()[0]*Piece.getDimensions()[1];
+				/*
+				**On cherche dans positionParcouru une position sur laquelle il reste de la saleté, la plus proche et différente de la position courante
+				*/
+				for(int j=0;j<positionParcouru.size();j++){
+					if(!Piece.getPosition(positionParcouru.get(j).getX(), positionParcouru.get(j).getY()).equals(positionCourante)
+						&&Piece.getPosition(positionParcouru.get(j).getX(),positionParcouru.get(j).getY()).getQtePoussiere()>0){
+						if(distance>shortestDistanceBetweenPoints(positionCourante.getX(), positionCourante.getY(), positionParcouru.get(j).getX(), positionParcouru.get(j).getY())){
+							index=j;
+						}
+					}
+				}
+				
+				if(index>0){//Si on a trouvé une case qui n'est pas encore complétement nettoyé et qui a été nettoyé
+					PositionParcouru aim=positionParcouru.get(index);
+					boolean aChange=false;
+					if(aim.getX()>positionCourante.getX()){
+						if(droit.isAccessible(positionCourante)){
+							currentX++;
+							aChange=true;
+						}
+					}
+					else if(aim.getY()>positionCourante.getY()){
+						if(haut.isAccessible(positionCourante)){
+							currentY++;
+							aChange=true;
+						}
+					}
+					if(!aChange){
+						if(aim.getX()<positionCourante.getX()){
+							if(gauche.isAccessible(positionCourante)){
+								currentX--;
+							}
+						}
+						else{
+							if(bas.isAccessible(positionCourante)){
+								currentY--;
+							}
+						}
+					}
+					Position courante=Piece.getPosition(currentX, currentY);
+					PositionParcouru toAdd=new PositionParcouru(courante.getX(),courante.getY());
+					setPositionCourante(courante);
+					if(!positionParcouru.contains(courante)){
+						positionParcouru.add(toAdd);
+					}
+				}else{//On explore à nouveau la pièce
+					boolean access=false;
+					switch(directionCourante){
+						case 0:
+							access=haut.isAccessible(positionCourante);
+							currentY++;
+							break;
+						case 1:
+							access=droit.isAccessible(positionCourante);
+							currentX++;
+							break;
+						case 2:
+							access=gauche.isAccessible(positionCourante);
+							currentX--;
+							break;
+						case 3:
+							access=bas.isAccessible(positionCourante);
+							currentY--;
+							break;
+					}
+					if(access){		
+						Position courante=Piece.getPosition(currentX, currentY);
+						PositionParcouru toAdd=new PositionParcouru(courante.getX(),courante.getY());		
+						if(!positionParcouru.contains(courante)){
+							positionParcouru.add(toAdd);
+						}
+						setPositionCourante(courante);
+						
+					}
+					else{
+						findNewDirection();
+						bouger();
+					}
+				}
+				
+			}
+			else{
+				boolean access=false;
+				switch(directionCourante){
+					case 0:
+						access=haut.isAccessible(positionCourante);
+						currentY++;
+						break;
+					case 1:
+						access=droit.isAccessible(positionCourante);
+						currentX++;
+						break;
+					case 2:
+						access=gauche.isAccessible(positionCourante);
+						currentX--;
+						break;
+					case 3:
+						access=bas.isAccessible(positionCourante);
+						currentY--;
+						break;
+				}
+				if(access){
+					Position courante=Piece.getPosition(currentX, currentY);
+					PositionParcouru toAdd=new PositionParcouru(courante.getX(),courante.getY());
+					if(!positionParcouru.contains(courante)){
+						positionParcouru.add(toAdd);
+						setPositionCourante(courante);
+					}else{
+						if(droit.isAccessible(positionCourante) && !positionParcouru.contains(new PositionParcouru(positionCourante.getX()+1, positionCourante.getY()))){
+							positionParcouru.add(new PositionParcouru(positionCourante.getX()+1, positionCourante.getY()));
+							courante=Piece.getPosition(positionCourante.getX()+1, positionCourante.getY());
+							setPositionCourante(courante);
+							return;
+						}
+						if(gauche.isAccessible(positionCourante) && !positionParcouru.contains(new PositionParcouru(positionCourante.getX()-1, positionCourante.getY()))){
+							positionParcouru.add(new PositionParcouru(positionCourante.getX()-1, positionCourante.getY()));
+							courante=Piece.getPosition(positionCourante.getX()-1, positionCourante.getY());
+							setPositionCourante(courante);
+							return;
+						}
+						if(haut.isAccessible(positionCourante) && !positionParcouru.contains(new PositionParcouru(positionCourante.getX(), positionCourante.getY()+1))){
+							positionParcouru.add(new PositionParcouru(positionCourante.getX(), positionCourante.getY()+1));
+							courante=Piece.getPosition(positionCourante.getX(), positionCourante.getY()+1);
+							setPositionCourante(courante);
+							return;
+						}
+						if(bas.isAccessible(positionCourante) && !positionParcouru.contains(new PositionParcouru(positionCourante.getX(), positionCourante.getY()-1))){
+							positionParcouru.add(new PositionParcouru(positionCourante.getX(), positionCourante.getY()-1));
+							courante=Piece.getPosition(positionCourante.getX(), positionCourante.getY()-1);
+							setPositionCourante(courante);
+							return;
+						}
+					}
+					setPositionCourante(courante);
+				}
+				else{
+					findNewDirection();
+					bouger();
+				}
+			}
+		}
+		else{ //Démarrage du robot
+			
+			boolean access=false;
+			switch(directionCourante){
+				case 0:
+					access=haut.isAccessible(positionCourante);
+					currentY++;
+					break;
+				case 1:
+					access=droit.isAccessible(positionCourante);
+					currentX++;
+					break;
+				case 2:
+					access=gauche.isAccessible(positionCourante);
+					currentX--;
+					break;
+				case 3:
+					access=bas.isAccessible(positionCourante);
+					currentY--;
+					break;
+			}
+			if(!access){
+				findNewDirection();
+				bouger();
+				return;
+			}
+			else{
+				Position courante=Piece.getPosition(currentX, currentY);
+				PositionParcouru toAdd=new PositionParcouru(courante.getX(),courante.getY());
+				setPositionCourante(courante);
+				if(!positionParcouru.contains(courante)){
+					positionParcouru.add(toAdd);
+				}
+			}
+		}
+	}
+	/*public void bouger() throws InterruptedException, outOfEnergy{
 		boolean access=true;
 		int currentX=positionCourante.getX();
 		int currentY=positionCourante.getY();
@@ -244,13 +441,12 @@ public class Robot extends Thread{
 			if(!positionParcouru.contains(toAdd)){
 				positionParcouru.add(toAdd);			
 			}
-			//positionCourante.afficher();
 			if(estBloque){
 				findNewDirection();//ICI
 				estBloque=false;
 			}
 		}
-	}
+	}*/
 	/**
 	 * Retourne l'ensemble des positions accessibles depuis (x,y) 
 	 * 
@@ -303,17 +499,22 @@ public class Robot extends Thread{
 	 * @return Un booléen, vrai s'il en existe, faux sinon
 	 */
 	private boolean existDirectPath(int x, int y, ArrayList<PositionParcouru> aCheck, int previousX, int previousY){
+		boolean side=false;
+		boolean bottom=false;
 		for(int i=0;i<aCheck.size();i++){
 			if(	(aCheck.get(i).getX()+1==x && aCheck.get(i).getY()==y) ||
-				(aCheck.get(i).getX()-1==x && aCheck.get(i).getY()==y) ||
-				(aCheck.get(i).getX()==x && aCheck.get(i).getY()+1==y) ||
-				(aCheck.get(i).getX()==x && aCheck.get(i).getY()-1==y)&&
-				(aCheck.get(i).getX()!=previousX || aCheck.get(i).getY()!=previousY)
+					(aCheck.get(i).getX()-1==x && aCheck.get(i).getY()==y)&&
+					(aCheck.get(i).getX()!=previousX || aCheck.get(i).getY()!=previousY)){
+				side=true;
+			}
+			if((aCheck.get(i).getX()==x && aCheck.get(i).getY()+1==y) ||
+					(aCheck.get(i).getX()==x && aCheck.get(i).getY()-1==y)&&
+					(aCheck.get(i).getX()!=previousX || aCheck.get(i).getY()!=previousY)
 				){
-				return true;
+				bottom=true;
 			}
 		}
-		return false;
+		return side&&bottom;
 	}
 	
 	/**
@@ -324,11 +525,25 @@ public class Robot extends Thread{
 	 * @return Le chemin et un booleen indiquant si le chemin mène bien à la base. Sinon, le chemin n'est pas utilisable(Impasse).
 	 * @throws InterruptedException
 	 */
-	private ResultatRecurrence determinerPlusCourtCheminBase(int courantX, int courantY,ArrayList<PositionParcouru> cheminMinimal) throws InterruptedException{
+	private ResultatRecurrence determinerPlusCourtCheminBase(int courantX, int courantY,ArrayList<PositionParcouru> cheminMinimal,int departX, int departY) throws InterruptedException{
 		int baseX=getPositionBase().getX();
 		int baseY=getPositionBase().getY();
 		ArrayList<Integer> nextOnes=estAccessible(courantX, courantY);
-		Thread.sleep(100);
+	/*	if(positionCourante.getX()==4 && positionCourante.getY()==11){
+			System.out.println("Taille chem min ="+cheminMinimal.size());
+			System.out.println("Position courante : ("+courantX+","+courantY+")");
+			System.out.println("Next ones size = "+nextOnes.size());
+			for(int i=0;i<nextOnes.size();i++){
+				System.out.println("("+positionParcouru.get(nextOnes.get(i)).getX()+","+positionParcouru.get(nextOnes.get(i)).getY()+")");
+			}
+			System.out.print("Le chemin minimal c'est : ");
+			for(int j=0;j<cheminMinimal.size();j++){
+				System.out.print("("+cheminMinimal.get(j).getX()+","+cheminMinimal.get(j).getY()+")");
+			}
+			System.out.println("");
+			System.out.println("");
+			Robot.sleep(1000);
+		}*/
 		//System.out.println("Taille de nextOnes : "+nextOnes.size()+" pour ("+courantX+","+courantY+"), taille chemin : "+cheminMinimal.size());
 		if(nextOnes.size()>0 && (courantX!=baseX || courantY!=baseY)){
 			if(nextOnes.size()==1){
@@ -336,7 +551,7 @@ public class Robot extends Thread{
 					//System.out.println("Ajout de ("+courantX+","+courantY+") dans nextones==1");
 					cheminMinimal.add(positionParcouru.get(getPositionParcouru(courantX, courantY)));
 					PositionParcouru tempo=positionParcouru.get(nextOnes.get(0));
-					return determinerPlusCourtCheminBase(tempo.getX(),tempo.getY(),cheminMinimal);
+					return determinerPlusCourtCheminBase(tempo.getX(),tempo.getY(),cheminMinimal,departX,departY);
 				}
 				else{
 					return new ResultatRecurrence(cheminMinimal, false);
@@ -360,33 +575,37 @@ public class Robot extends Thread{
 					
 					nextOnes.remove(aTest);//ON LE SUPPRIME DES POINTS A TESTER
 					PositionParcouru tempo=positionParcouru.get(maValue);
+					//System.out.println("Valeur selectionné : ("+tempo.getX()+","+tempo.getY()+")");
 					if(!cheminMinimal.contains(tempo)){
 						if(!existDirectPath(tempo.getX(), tempo.getY(), cheminMinimal, courantX,courantY)){	
 							//System.out.println("Ajout de ("+positionParcouru.get(getPositionParcouru(courantX, courantY)).getX()+","+positionParcouru.get(getPositionParcouru(courantX, courantY)).getY()+") dans while !existDirectPath avec tempo("+tempo.getX()+","+tempo.getY()+")");
+						//	System.out.println("ENTREE");
 							cheminMinimal.add(positionParcouru.get(getPositionParcouru(courantX, courantY)));
-							ResultatRecurrence res=determinerPlusCourtCheminBase(tempo.getX(),tempo.getY(),cheminMinimal);
+							ResultatRecurrence res=determinerPlusCourtCheminBase(tempo.getX(),tempo.getY(),cheminMinimal,departX,departY);
 							if(res.isEstTermine()){
 								return res;
 							}
 							else{
+							//	System.out.println("ICI");
 								//System.out.println("Suppresion de ("+cheminMinimal.get(cheminMinimal.size()-1).getX()+","+cheminMinimal.get(cheminMinimal.size()-1).getY()+") avec tempo("+tempo.getX()+","+tempo.getY()+")");
 								cheminMinimal.remove(cheminMinimal.size()-1);
 							}
 						}
 						else{
-							//System.out.println("Suppresion de ("+cheminMinimal.get(cheminMinimal.size()-1).getX()+","+cheminMinimal.get(cheminMinimal.size()-1).getY()+")");
+							//System.out.println("Suppression de ("+cheminMinimal.get(cheminMinimal.size()-1).getX()+","+cheminMinimal.get(cheminMinimal.size()-1).getY()+")");
 							cheminMinimal.remove(cheminMinimal.size()-1);
-							ResultatRecurrence res=determinerPlusCourtCheminBase(tempo.getX(),tempo.getY(),cheminMinimal);
+							ResultatRecurrence res=determinerPlusCourtCheminBase(tempo.getX(),tempo.getY(),cheminMinimal,departX,departY);
 							if(res.isEstTermine()){
 								return res;
 							}
 							else{
-								//System.out.println("Suppresion de ("+cheminMinimal.get(cheminMinimal.size()-1).getX()+","+cheminMinimal.get(cheminMinimal.size()-1).getY()+") avec tempo("+tempo.getX()+","+tempo.getY()+")");
+							//	System.out.println("Suppression de ("+cheminMinimal.get(cheminMinimal.size()-1).getX()+","+cheminMinimal.get(cheminMinimal.size()-1).getY()+") avec tempo("+tempo.getX()+","+tempo.getY()+")");
 								cheminMinimal.remove(cheminMinimal.size()-1);
 							}
 						}
 					}
 				}
+				//System.out.println("("+courantX+","+courantY+") retourne faux");
 				return new ResultatRecurrence(cheminMinimal, false);
 			}
 		}
@@ -396,6 +615,7 @@ public class Robot extends Thread{
 			return new ResultatRecurrence(cheminMinimal,true);
 		}
 		else{
+			//System.out.println("("+courantX+","+courantY+") retourne faux");
 			return new ResultatRecurrence(cheminMinimal, false);
 		}
 		
@@ -408,8 +628,9 @@ public class Robot extends Thread{
 	 * @return l'indice
 	 */
 	private int getPositionParcouru(int x, int y){
-		for(int i=0;i<positionParcouru.size();i++){
-			if(positionParcouru.get(i).getX()==x && positionParcouru.get(i).getY()==y){
+		ArrayList<PositionParcouru> tab=positionParcouru;
+		for(int i=0;i<tab.size();i++){
+			if(tab.get(i).getX()==x && tab.get(i).getY()==y){
 				return i;
 			}
 		}
@@ -547,7 +768,7 @@ public class Robot extends Thread{
 	 * @throws outOfEnergy
 	 */
 	public void setPositionCourante(Position positionCourante) throws InterruptedException, outOfEnergy {
-		Robot.sleep(250);
+		Robot.sleep(100);
 	
 		if(positionCourante.getType().charAt(0)=='T'){
 			if(this.capaciteBatterie-consommationBase*1.5>0){
